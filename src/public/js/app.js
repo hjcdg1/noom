9ac527f8ed326353bc4808ec71e89a85a93419fc
최덕경 <hjcdg197@gmail.com>
2022-05-22 14:57:31 +0900

    기본적인 채팅 기능 구현하기 (WebSocket 이용)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,6 @@
+const messageList = document.querySelector("ul");
+const nicknameForm = document.querySelector("#nickname");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
@@ -5,13 +8,33 @@ socket.addEventListener("open", () => {
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("New message:", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
   console.log("Disconnected from Server.");
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser!");
-}, 10000);
+function makeMessage(type, payload) {
+  const message = { type, payload };
+  return JSON.stringify(message);
+}
+
+function handleNicknameSubmit(event) {
+  event.preventDefault();
+  const input = nicknameForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value = "";
+}
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("message", input.value));
+  input.value = "";
+}
+
+nicknameForm.addEventListener("submit", handleNicknameSubmit);
+messageForm.addEventListener("submit", handleMessageSubmit);
--- src/server.js
+++ src/server.js
@@ -16,14 +16,26 @@ const server = http.createServer(app);
 server.listen(3000, handleListen);
 
 const wss = new WebSocket.Server({ server });
+const sockets = [];
 wss.on("connection", (socket) => {
+  socket["nickname"] = "Anonymous";
+  sockets.push(socket);
   console.log("Connected to Browser.");
 
-  socket.on("message", (message) =>
-    console.log("New message:", message.toString("utf8"))
-  );
+  socket.on("message", (message) => {
+    const messageObj = JSON.parse(message.toString("utf8"));
+
+    switch (messageObj.type) {
+      case "nickname":
+        socket["nickname"] = messageObj.payload;
+        break;
+      case "message":
+        sockets.forEach((_socket) =>
+          _socket.send(`${socket.nickname}: ${messageObj.payload}`)
+        );
+        break;
+    }
+  });
 
   socket.on("close", () => console.log("Disconnected from Browser."));
-
-  socket.send("hello from the server!");
 });
