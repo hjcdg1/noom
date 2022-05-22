0090d6f6c3ce601855983d596faec8afebd7b11f
최덕경 <hjcdg197@gmail.com>
2022-05-22 13:18:57 +0900

    HTTP vs WebSocket

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import WebSocket from "ws";
+import http from "http";
 
 const app = express();
 
@@ -9,4 +11,19 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+server.listen(3000, handleListen);
+
+const wss = new WebSocket.Server({ server });
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser.");
+
+  socket.on("message", (message) =>
+    console.log("New message:", message.toString("utf8"))
+  );
+
+  socket.on("close", () => console.log("Disconnected from Browser."));
+
+  socket.send("hello from the server!");
+});
